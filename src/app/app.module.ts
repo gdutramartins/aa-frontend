81bb425f8cf6581c647300115559210d3ff52243
gdutramartins <gdutramartins@gmail.com>
2022-12-12 05:56:42 -0300

    tentando login

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { AppService } from './app.service';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home..component';
+import { Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home'},
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginComponent}
+];
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [
+    AppService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
